d5315ce472cd7aa6f4d20f374b45806c0b941668
Yeri <kailynx@gmail.com>
2013-04-18 22:32:03 +1000

    add testGenerator~~()

--- src/simulator/Tests.java
+++ src/simulator/Tests.java
@@ -112,18 +112,18 @@ public class Tests {
 		
 	}
 	*/
-   @Test
-   public void testOrderBookGetBid() {
-	   Order bid1 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.76, 1959.0, new String(), new Long(0), new Long("6239925033925459786"), new Long(0), "B");
-	   Order bid2 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.720, 15,new String(), new Long(0),new Long("6239925033924850752"), new Long(0),"B");
-	   Order bid3 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.380,111,new String(),new Long(0), new Long("6239925033923871154"), new Long(0),"B");
-	   OrderBooks b = factory.makeOrderBooks(); 
-	   b.processOrder(bid1);
-	   b.processOrder(bid2);
-	   b.processOrder(bid3);
-	   assertEquals(3, b.bidListSize());
-	   assertEquals(bid1, b.bestBidOrder());
-   }
+    @Test
+    public void testOrderBookGetBid() {
+	    Order bid1 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.76, 1959.0, new String(), new Long(0), new Long("6239925033925459786"), new Long(0), "B");
+	    Order bid2 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.760, 15,new String(), new Long(0),new Long("6239925033924850752"), new Long(0),"B");
+	    Order bid3 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.380,111,new String(),new Long(0), new Long("6239925033923871154"), new Long(0),"B");
+	    OrderBooks b = factory.makeOrderBooks(); 
+	    b.processOrder(bid1);
+	    b.processOrder(bid2);
+	    b.processOrder(bid3);
+	    assertEquals(3, b.bidListSize());
+	    assertEquals(bid1, b.bestBidOrder());
+    }
 	
 	@Test
 	public void testOrderBookGetAsk() {		
@@ -145,11 +145,17 @@ public class Tests {
 		b.processOrder(bid);
 		assertEquals(1, b.bidListSize());
 		assertEquals(bid, b.bestBidOrder());
-		
-		Order ask = new Order("CBA", "20130304" ,"00:00:00.000","ENTER", 67.900, 409,new String(),new Long(0), new Long(0), new Long("6238329642550435411"), "A");
-		b.processOrder(ask);
+		Order bid2 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.760, 15,new String(), new Long(0),new Long("6239925033924850752"), new Long(0),"B");
+	    b.processOrder(bid2);
+	    assertEquals(2, b.bidListSize());
+	    
+		Order ask1 = new Order("CBA", "20130304" ,"00:00:00.000","ENTER", 67.900, 409,new String(),new Long(0), new Long(0), new Long("6238329642550435411"), "A");
+		Order ask2 = new Order("CBA", "20130304", "00:00:00.000", "ENTER", 67.900, 200, new String(), new Long(0), new Long(0), new Long("6239528659982899884"), "A");
+		b.processOrder(ask1);
 		assertEquals(1, b.askListSize());
-		assertEquals(ask, b.bestAskOrder());
+		assertEquals(ask1, b.bestAskOrder());
+		b.processOrder(ask2);
+		assertEquals(2, b.askListSize());
 	}
 	
 	@Test
@@ -174,51 +180,108 @@ public class Tests {
 	
 	@Test
 	public void testOrderBookAmend() {
-		// unimplemented yet
+		// related functions are implementing
 	}
 	
 	@Test
 	public void testOrderBookTrade() {
-		// unimplemented yet
+		// related functions are implementing
 	}
 	
 	@Test
 	public void testOrderBookOffTrade() {
-		// unimplemented yet
+		// related functions are implementing
 	}
 	
 	@Test
 	public void testOrderBookCancelTrade() {
-		// unimplemented yet
+		// related functions are implementing
 	}
-	
+
+/*	
 	@Test
 	public void testGeneratorGetTimeStamp() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected date, sg.advance().date());
 	}
 
 	@Test
 	public void testGeneratorGetRecordType() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected type, sg.advance().recordType());
 	}
 
 	@Test
 	public void testGeneratorGetPrice() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected price, sg.advance().price());
 	}
 
 	@Test
 	public void testGeneratorGetVolume() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected volume, sg.advance().volume());
 	}
 
 	@Test
 	public void testGeneratorGetBidAsk() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected bidAsk, sg.advance().bidAsk());
 	}
 	
 	@Test
 	public void testGeneratorGetQualifier() {
-
+		Reader CSV = null;
+		try {
+			CSV = factory.makeReader(sampleFilePath);
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Strategy strategy= new Strategy(choice);
+		SignalGenerator sg = new SignalGenerator(CSV, strategy);
+		assertEquals(expected qualifiers, sg.advance().qualifiers());
 	}
+*/
 }
+
