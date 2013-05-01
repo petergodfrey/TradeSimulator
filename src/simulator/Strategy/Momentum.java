0716a59f4edb901f96b4cf93cff83f56d5637826
Peter Godfrey <peter.godfrey11@gmail.com>
2013-05-01 13:26:37 +1000

    Add Momentum strategy

---
+++ src/simulator/Strategy/Momentum.java
@@ -0,0 +1,25 @@
+package simulator.strategy;
+
+import simulator.Order;
+import simulator.OrderBooks;
+
+public class Momentum extends AbstractStrategy implements Strategy {
+
+	public Momentum(OrderBooks books) {
+		super(books);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Order strategise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStrategyName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
