2d5d76d4efb83bd2d7495527200ea82318bb610c
Jason Huang <mrjaayson@gmail.com>
2013-05-14 01:24:29 +1000

    wrote tests for one day of a small company to ensure that our simulation runs properly on basic data

--- src/test/TestSimulation.java
+++ src/test/TestSimulation.java
@@ -17,6 +17,7 @@ import simulator.Order;
 import simulator.OrderBooks;
 import simulator.Reader;
 import simulator.SignalGenerator;
+import simulator.Trade;
 import simulator.TradeEngine;
 import simulator.strategy.Strategy;
 
@@ -55,7 +56,7 @@ public class TestSimulation {
 		}
 
 		Order o;
-		
+
 		while ((o = SG.advance()) != null) {
 			orderBooks.processOrder(o);
 			tradeEngine.trade();
@@ -80,7 +81,7 @@ public class TestSimulation {
 		}
 
 		Order o;
-		
+
 		while ((o = SG.advance()) != null) {
 			orderBooks.processOrder(o);
 			tradeEngine.trade();
@@ -140,28 +141,251 @@ public class TestSimulation {
 		}
 
 		Order o;
+		int counter = 0;
 		while ((o = SG.advance()) != null) {
 			orderBooks.processOrder(o);
 			tradeEngine.trade();
-			testUniqueness(orderBooks);
-			
-			
+			System.out.println(counter++);
+			testOrderBookIDUniqueness(orderBooks);
+
+
 		}
 	}
-	
-	private void testUniqueness(OrderBooks books) {
-		
+
+	private void testOrderBookIDUniqueness(OrderBooks books) {
+
 		List<Order> bid = books.bidList();
 		List<Order> ask = books.askList();
 		Set<Long> bidSet = new HashSet<Long>();
 		Set<Long> askSet = new HashSet<Long>();
-		for (int i = 1; i < bid.size(); i++) {
+		for (int i = 0; i < bid.size(); i++) {
+			//check that it is not currently in the set
+			//add it in if not
 			assertFalse(bidSet.contains(bid.get(i).ID()));
 			bidSet.add(bid.get(i).ID());
 		}
-		for (int i = 1; i < ask.size(); i++) {
+		for (int i = 0; i < ask.size(); i++) {
 			assertFalse(askSet.contains(ask.get(i).ID()));
 			askSet.add(ask.get(i).ID());
 		}
+	}
+
+	@Test
+	public void testManualInputs() {
+
+		System.out.println("Testing manualInputs");
+		Order o0 = new Order("00:00:00.000","ENTER",0.040,100000,""  ,new Long("6224475796041657973"),"B");
+		Order o1 = new Order("00:00:00.000","ENTER",0.075, 41024,""  ,new Long("6224475796041587834"),"A");
+		Order o2 = new Order("00:00:00.000","ENTER",0.078, 45000,""  ,new Long("6225279539040458763"),"A");
+		Order o3 = new Order("00:00:00.000","ENTER",0.094, 39998,""  ,new Long("6219341351617764647"),"A");
+		Order o4 = new Order("13:08:34.603","ENTER",0.074, 10000,""  ,new Long("6230017334645827603"),"A");
+		Order o5 = new Order("13:13:19.953","ENTER",0.074, 55000,""  ,new Long("6230017334645856696"),"B");
+		Trade t0 = new Trade("13:13:19.953","TRADE",0.074, 10000,"",new Long("0"), "", o5,o4);
+		Order o6 = new Order("13:14:37.327","AMEND",0.074, 45000,""  ,new Long("6225279539040458763"),"A");
+		Trade t1 = new Trade("13:14:37.327","TRADE",0.074, 45000,"",new Long("1"), "", o5,o6);
+
+		{
+			//initial conditions
+			assertEquals(orderBooks.bidListSize(), 0);
+			assertEquals(orderBooks.askListSize(), 0);
+		}
+		{
+			//process first order into orderbooks
+			orderBooks.processOrder(o0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 0);
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 0);
+		}
+		{
+			//process second order into orderbooks
+			orderBooks.processOrder(o1);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 1);
+			compareOrders(orderBooks.askList().get(0), (o1));
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 1);
+			compareOrders(orderBooks.askList().get(0), (o1));
+		}
+		{
+			//process third order into orderbooks
+			orderBooks.processOrder(o2);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 2);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o2));
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 2);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o2));
+		}
+		{
+			//process fourth order into orderbooks
+			orderBooks.processOrder(o3);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 3);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o2));
+			compareOrders(orderBooks.askList().get(2), (o3));
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 3);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o2));
+			compareOrders(orderBooks.askList().get(2), (o3));
+		}
+		{
+			//process fifth order into orderbooks
+			orderBooks.processOrder(o4);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 4);
+			compareOrders(orderBooks.askList().get(0), (o4));
+			compareOrders(orderBooks.askList().get(1), (o1));
+			compareOrders(orderBooks.askList().get(2), (o2));
+			compareOrders(orderBooks.askList().get(3), (o3));
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),0);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+
+			assertEquals(orderBooks.askList().size(), 4);
+			compareOrders(orderBooks.askList().get(0), (o4));
+			compareOrders(orderBooks.askList().get(1), (o1));
+			compareOrders(orderBooks.askList().get(2), (o2));
+			compareOrders(orderBooks.askList().get(3), (o3));
+		}
+		{
+			//process sixth order into orderbooks
+			orderBooks.processOrder(o5);
+
+			assertEquals(orderBooks.bidList().size(), 2);
+			compareOrders(orderBooks.bidList().get(0), (o5));
+			compareOrders(orderBooks.bidList().get(1), (o0));
+			//prior to trade, the volume it has
+			assertEquals(orderBooks.bidList().get(0).volume(), 55000);
+
+			assertEquals(orderBooks.askList().size(), 4);
+			compareOrders(orderBooks.askList().get(0), (o4));
+			compareOrders(orderBooks.askList().get(1), (o1));
+			compareOrders(orderBooks.askList().get(2), (o2));
+			compareOrders(orderBooks.askList().get(3), (o3));
+			//prior to trade, the volume it has
+			assertEquals(orderBooks.askList().get(0).volume(), 10000);
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),1);
+			compareTrades(tradeEngine.getTradeList().get(0),t0);
+
+			assertEquals(orderBooks.bidList().size(), 2);
+			compareOrders(orderBooks.bidList().get(0), (o5));
+			compareOrders(orderBooks.bidList().get(1), (o0));
+			assertEquals(orderBooks.bidList().get(0).volume(), 45000);
+
+			assertEquals(orderBooks.askList().size(), 3);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o2));
+			compareOrders(orderBooks.askList().get(2), (o3));
+		}
+		{
+			//process seventh order into orderbooks
+			orderBooks.processOrder(o6);
+
+			assertEquals(orderBooks.bidList().size(), 2);
+			compareOrders(orderBooks.bidList().get(0), (o5));
+			compareOrders(orderBooks.bidList().get(1), (o0));
+			//prior to trade, the volume it has
+			assertEquals(orderBooks.bidList().get(0).volume(), 45000);
+
+			assertEquals(orderBooks.askList().size(), 3);
+			compareOrders(orderBooks.askList().get(0), (o6));
+			compareOrders(orderBooks.askList().get(1), (o1));
+			compareOrders(orderBooks.askList().get(2), (o3));
+			//prior to trade, the volume it has
+			assertEquals(orderBooks.askList().get(0).volume(), 45000);
+		}
+		{
+			//attempt to trade
+			tradeEngine.trade();
+			assertEquals(tradeEngine.getTradeList().size(),2);
+			compareTrades(tradeEngine.getTradeList().get(0),t0);
+			compareTrades(tradeEngine.getTradeList().get(1),t1);
+
+			assertEquals(orderBooks.bidList().size(), 1);
+			compareOrders(orderBooks.bidList().get(0), (o0));
+			assertEquals(orderBooks.bidList().get(0).volume(), 100000);
+
+			assertEquals(orderBooks.askList().size(), 2);
+			compareOrders(orderBooks.askList().get(0), (o1));
+			compareOrders(orderBooks.askList().get(1), (o3));
+		}
+	}
+
+
+	private void compareOrders(Order o1, Order o2) {
+		assertEquals(o1.time(), o2.time());
+		assertEquals(o1.recordType(), o2.recordType());
+		assertEquals(o1.price(), o2.price());
+		assertEquals(o1.volume(), o2.volume());
+		assertEquals(o1.qualifiers(), o2.qualifiers());
+		assertEquals(o1.ID(), o2.ID());
+		assertEquals(o1.bidAsk(), o2.bidAsk());
+	}
+	
+	private void compareTrades(Trade t1, Trade t2) {
+		compareOrders(t1, t2);
+		assertEquals(t1.getBid(), t2.getBid());
+		assertEquals(t1.getAsk(), t2.getAsk());
 	}
 }
