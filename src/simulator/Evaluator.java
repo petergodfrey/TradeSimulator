5c22b5370cf244af8c156ac75ffe569fd586aac5
Jason Huang <mrjaayson@gmail.com>
2013-05-27 22:48:12 +1000

    modified strategies and added some test data

--- src/simulator/Evaluator.java
+++ src/simulator/Evaluator.java
@@ -33,15 +33,23 @@ public class Evaluator {
 	
 	public int calculateProfit(List<Trade> strategyTrades) {
 		int profit = 0;
-
+		int totalBuy = 0, totalSell = 0;
 		for (Trade t:strategyTrades) {
 			if (t.getAsk().ID().compareTo(BigInteger.ZERO) < 0) {
 				profit += t.volume() * t.price();
+				totalSell += t.volume() * t.price();
 			}
 			if (t.getBid().ID().compareTo(BigInteger.ZERO) < 0) {
 				profit -= t.volume() * t.price();
+				totalBuy += t.volume() * t.price();
 			}
 		}
+		System.out.println("totalBuy");
+		System.out.println(totalBuy);
+		System.out.println("totalSell");
+		System.out.println(totalSell);
+		System.out.println("percentage");
+		System.out.println(((double)(totalSell)/(double)(totalBuy)));
 		return profit;
 	}
 
--- src/simulator/Strategy/AbstractStrategy.java
+++ src/simulator/Strategy/AbstractStrategy.java
@@ -9,50 +9,52 @@ import simulator.OrderBooks;
 
 
 public abstract class AbstractStrategy implements Strategy {
-	
+
 	protected OrderBooks books;
 
 	private BigInteger IDCounter = BigInteger.ONE.negate();// == -1
-	
-	private List<Order> stratOrders = new ArrayList<Order>();
-	
+
+	protected List<Order> stratOrders = new ArrayList<Order>();
+
 	public AbstractStrategy(OrderBooks books) {
 		this.books = books;
 	}
-	
+
 	private BigInteger generateStrategyID() {
 		BigInteger returnInt = IDCounter;
 		IDCounter = IDCounter.add(BigInteger.ONE.negate());// -= 1
 		return returnInt;
 	}
-	
+
 	@Override
 	public Order submitOrder() {
 		Order o = strategise();
 		addOrderToList(o);
 		return o;
 	}
-	
+
 	private void addOrderToList(Order o) {
-		stratOrders.add(o);
+		if (o != null) {
+			stratOrders.add(0, o);
+		}
 	}
-	
+
 	@Override
 	public List<Order> getOrderList() {
 		return this.stratOrders;
 	}
-	
+
 
 	protected Order createOrder(
-							String recordType,
-							Double price,
-							int volume,
-							String qualifer,
-							String bidAsk) {
+			String recordType,
+			Double price,
+			int volume,
+			String qualifer,
+			String bidAsk) {
 		return new Order(books.getSimulatedTime(),recordType, price, volume, qualifer,
 				generateStrategyID(), bidAsk);
 	}
-	
+
 	@Override
 	public void reset() {
 		IDCounter = BigInteger.ONE.negate();
